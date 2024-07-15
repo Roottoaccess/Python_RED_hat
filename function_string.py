ee4d18f3f343c006f592ec8a99f13e177f222287
NetworkCrab <network123crab@gmail.com>
2024-07-15 17:59:30 +0530

    This are the string funtion

---
+++ function_string.py
@@ -0,0 +1,23 @@
+name = "coffee"
+
+# For finding the length of the string
+print(len(name))
+
+# Return boolean
+print(name.endswith("fee"))
+
+# Startswith function
+print(name.startswith("Co"))
+
+# Capitalize string
+print(name.capitalize())
+
+# Find function
+s = "java dns"
+index = s.find("dns")
+# After find it will return the index from where the word will start
+print(index)
+
+# Replace method
+s1 = "This is a python tutorial"
+print(s1.replace("tutorial","hack"))
