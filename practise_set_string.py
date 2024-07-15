67727bd0c492858e934d66f834ebc6ba334ec256
NetworkCrab <network123crab@gmail.com>
2024-07-15 18:21:16 +0530

    This is the practise set of the string course....

---
+++ practise_set_string.py
@@ -0,0 +1,28 @@
+# String python practise set
+
+# Question 1
+# Taking the input from the user
+# name = input("Enter your name: ")
+
+# print(f"Good afternoon {name}")
+
+
+# Question 2
+# letter = '''Dear <|Name|>,
+# You are selected!
+# <|Date|>'''
+# Chaining of the replace
+# print(letter.replace("<|Name|>", "Biswarup Dutta").replace("<|Date|>", "15 July"))
+
+
+# Question 3
+# Detecting double space in a string
+# name = "Harry is a good  boy"
+# print(name.find("  "))
+# Replacing the double spac to single space
+# print(name.replace("  "," "))
+
+# Question 4
+# using excape sequence character
+letter = "Dear Jeet,\n\t This python course is nice.\n Thanks!"
+print(letter)
