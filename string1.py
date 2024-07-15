1ea4593ea4e16280f2f2381ca3268006dd749bdc
NetworkCrab <network123crab@gmail.com>
2024-07-15 17:42:34 +0530

    core string concept in python

---
+++ string1.py
@@ -0,0 +1,33 @@
+name = "Java Coding"
+
+print(name)
+
+# Type of the variable
+print(type(name))
+
+# String are immutable
+
+# Length of the string
+print(len(name))
+
+# Slicing of the string
+nameshort = name[0:3] # Start from index 0 all the way till 3(excluding 3)
+
+
+print("After slicing the string: ",nameshort)
+
+# Negative slicing in the python
+pet = "Tiger"
+
+print(pet)
+
+# Negative slicing
+print(pet[-4: -1])
+# Converting the negative index into corresponding positive index
+print(pet[1: 4])
+
+
+# Slicing with skip value
+word =  "012345678"
+print(word[1: 6: 2])
+
