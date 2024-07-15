af5f23a245b285dafad4e40b4a51dddf7f111060
NetworkCrab <network123crab@gmail.com>
2024-07-15 19:09:54 +0530

    This is the list and tupple practise set........

---
+++ practise_set_list_tupple.py
@@ -0,0 +1,48 @@
+# Question 1
+# Declaring the empty list
+# fruits = []
+# Taking the input from the user
+# f1 = input("Enter fruits name: ")
+# fruits.append(f1)
+# f2 = input("Enter fruits name: ")
+# fruits.append(f2)
+# f3 = input("Enter fruits name: ")
+# fruits.append(f3)
+# f4 = input("Enter fruits name: ")
+# fruits.append(f4)
+# f5 = input("Enter fruits name: ")
+# fruits.append(f5)
+# f6 = input("Enter fruits name: ")
+# fruits.append(f6)
+# f7 = input("Enter fruits name: ")
+# fruits.append(f7)
+# f8 = input("Enter fruits name: ")
+# fruits.append(f8)
+# Printing the result
+# print(fruits) 
+
+# Question 2
+# marks = []
+# m1 = int(input("Enter the marks: "))
+# marks.append(m1)
+# m2 = int(input("Enter the marks: "))
+# marks.append(m2)
+# m3 = int(input("Enter the marks: "))
+# marks.append(m3)
+# m4 = int(input("Enter the marks: "))
+# marks.append(m4)
+# m5 = int(input("Enter the marks: "))
+# marks.append(m5)
+# m6 = int(input("Enter the marks: "))
+# marks.append(m6)
+
+# marks.sort()
+# print(marks)
+
+# Question 3
+# l = [34, 24, 645, 124]
+# print(sum(l))
+
+# Qustion 4
+a = (7, 0, 8, 0, 0, 9)
+print(a.count(0))
