6e5b31464697bace939496324d7631eff193a1ad
NetworkCrab <network123crab@gmail.com>
2024-07-19 16:21:36 +0530

    This is the practise set of python set and dictionary program

---
+++ set_dict_practise_set.py
@@ -0,0 +1,82 @@
+# This is the python script to solve the dictionary and set practise questions
+
+
+# Question 1
+# words = {
+#     "madat": "help",
+#     "kursi": "chair",
+#     "billi": "cat",
+#     "kutta": "dog"
+# }
+
+# word = input("Enter the wor you want meaning of: ")
+
+
+# print(words[word])
+
+
+# Question 2
+# n = int(input("Enter number 1: "))
+# n1 = int(input("Enter number 2: "))
+# n2 = int(input("Enter number 2: "))
+# n3 = int(input("Enter number 3: "))
+# n4 = int(input("Enter number 4: "))
+# n5 = int(input("Enter number 5: "))
+# n6 = int(input("Enter number 6: "))
+# n7 = int(input("Enter number 7: "))
+
+# inp = {n , n1 , n2, n3, n4, n5, n6, n7}
+# print(inp, type(inp))
+
+
+# Question 3
+# s = set()
+# s.add(18)
+# s.add("18")
+
+# print(s)
+
+# Question 4
+# s = set()
+# s.add(20)
+# s.add(20.0) # 20.0 and 20 both are same
+# s.add('20')
+
+# print(s)
+
+
+# Question 5
+# s = {} empty dictionary
+
+# Question 6
+# dic = {}
+
+# name = input("Enter friends name: ")
+# lang = input("Enter Language name: ")
+
+# dic.update({name: lang})
+
+# name = input("Enter friends name: ")
+# lang = input("Enter Language name: ")
+
+# dic.update({name: lang})
+
+# name = input("Enter friends name: ")
+# lang = input("Enter Language name: ")
+
+# dic.update({name: lang})
+
+# name = input("Enter friends name: ")
+# lang = input("Enter Language name: ")
+
+# # This is use to add in the dictionary
+# dic.update({name: lang})
+
+
+# print(dic)
+
+# Question 9
+# s = {8, 7, 12, "Harry", [1,2]}
+
+# We can not update a set you cannot have a list in the list as well
+# They are not hashable
