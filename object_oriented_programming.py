f7606bc4c846e644786acfe85f2b2b31c626888b
NetworkCrab <network123crab@gmail.com>
2024-07-24 21:42:50 +0530

    this is object oriented programming in python covered full in java.....

---
+++ object_oriented_programming.py
@@ -0,0 +1,13 @@
+# This is the object oriented programming with python
+
+# DRY do not repeat yourself
+
+class Employee:
+    name = "Harry"
+    language = "Python"
+    salary = 1200000
+
+
+harry = Employee()
+print(harry.name, harry.language, harry.salary)
+
